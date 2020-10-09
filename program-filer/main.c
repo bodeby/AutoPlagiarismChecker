9310fc4a898f86953884e28c865becb421929b55
bodeby <thorbensen@gmail.com>
2020-10-09 10:52:37 +0200

    Created Start File

--- program-filer/main.c
+++ program-filer/main.c
@@ -1,5 +1,18 @@
+/*
+* Titel: Automatisk Plagiat Kontrol
+* Software - 1. semester (09/10/2020 - 25/12/2020)
+* Aalborg universitet CPH
+* 
+* Gruppe: SW0001
+* - Markus Hye Knudsen, Oscar Maxwell
+* - Sara Granquist, Tommy Grenaae
+* - Alberte Østergaard Andesen, Frederik Bode Thorbensen
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
 int main(void) {
 
-    printf("Hej");
-    return 0;
+    return EXIT_SUCCESS;
 }
