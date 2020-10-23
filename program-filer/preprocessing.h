6263171ab5d5d9974c5af966215c3277171b990e
granquist8 <granquist8@gmail.com>
2020-10-23 12:40:01 +0200

    processingStart

---
+++ program-filer/preprocessing.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void preprocessing (char *navn_paa_fil) 
+{
+    int text_size=10; /*Ændre størrelsen!!*/
+    char text[text_size]; 
+    strlen(text);
+
+    for (int i = 0; i<=text_size; i++) 
+    {
+        text[i]
+
+    }
+}
