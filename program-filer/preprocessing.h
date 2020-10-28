73333a34150e729f903606f69b46d60258e54c70
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2020-10-28 12:01:10 +0100

    add Test file

--- program-filer/preprocessing.h
+++ program-filer/preprocessing.h
@@ -6,7 +6,7 @@ void preprocessing (/*char *navn_paa_fil*/)
 
     char navn_paa_fil[100] = "Hej mit navn er Oscar.\n og jeg er 22";
     int text_size=strlen(navn_paa_fil);
-    char text[text_size]; 
+    char test[text_size]
     char p[text_size];
     int i=0;
 
@@ -14,9 +14,13 @@ void preprocessing (/*char *navn_paa_fil*/)
     {*/ //https://www.tutorialspoint.com/c_standard_library/c_function_strncpy.htm
         p[i]=*strncpy(navn_paa_fil,p,i);
         p[i+1]=*strncpy(navn_paa_fil,p,i+1);
-        char tekst1 = *strncpy(navn_paa_fil,p,i);
-        char tekst2 = *strncpy(navn_paa_fil,p,i+1);
-        printf("%c %c",tekst1,tekst2);
+
+        char tekst1[i];
+        char tekst2[i+1];
+
+        tekst1[i] = *strncpy(navn_paa_fil,p,i);
+        tekst2[i+1] = *strncpy(navn_paa_fil,p,i+1);
+        printf("%s %s",tekst1[i],tekst2[i+1]);
         /*
         if (p[i]=='\\') 
         {
