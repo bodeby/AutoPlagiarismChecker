16eafc8448dcb433795f46c540890593533b8a64
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2020-10-28 12:01:39 +0100

    add test file

--- program-filer/preprocessing.c
+++ program-filer/preprocessing.c
@@ -1,30 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-void preprocessing (/*char *navn_paa_fil*/) 
+int main (void) 
 {
 
     char navn_paa_fil[100] = "Hej mit navn er Oscar.\n og jeg er 22";
     int text_size=strlen(navn_paa_fil);
-<<<<<<< Updated upstream
-    char test[text_size]
-=======
 
->>>>>>> Stashed changes
     char p[text_size];
     int i=0;
 
     /*for (int i = 0; i<=text_size; i++) 
     {*/ //https://www.tutorialspoint.com/c_standard_library/c_function_strncpy.htm
-        p[i]=*strncpy(navn_paa_fil,p,i);
-        p[i+1]=*strncpy(navn_paa_fil,p,i+1);
 
-        char tekst1[i];
-        char tekst2[i+1];
 
-        tekst1[i] = *strncpy(navn_paa_fil,p,i);
-        tekst2[i+1] = *strncpy(navn_paa_fil,p,i+1);
-        printf("%s %s",tekst1[i],tekst2[i+1]);
+        strncpy(navn_paa_fil,p,i);
+        strncpy(navn_paa_fil,p,i+1);
+        printf("%s",p);
         /*
         if (p[i]=='\\') 
         {
