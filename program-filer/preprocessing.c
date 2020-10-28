ad385507826145c4431406ef645d14ef48f83dd0
OscarMaxwell <72600165+OscarMaxwell@users.noreply.github.com>
2020-10-28 17:35:51 +0100

    Rettelser

--- program-filer/preprocessing.c
+++ program-filer/preprocessing.c
@@ -4,12 +4,11 @@
 int main (void) 
 {
 
-    char navn_paa_ori_fil[100] = "Hej mit navn er Oscar.\nJeg er 22 aar.\nJeg er en dreng";
+    char navn_paa_ori_fil[100] = "Hej mit navn er Oscar.\nJeg er 22 aar.\nJeg går på uni.";
     int text_size1=strlen(navn_paa_ori_fil);
 
     char navn_paa_ny_fil[text_size1];
     strncpy (navn_paa_ny_fil, navn_paa_ori_fil, text_size1);
-    int text_size2=strlen(navn_paa_ny_fil);
 
     int tæller = 0;
     int tæller_punktom = 0;
@@ -25,10 +24,12 @@ int main (void)
         }
         tæller += 1;
     }
+    
+    int text_size2=strlen(navn_paa_ny_fil);
 
-    printf("\n%d %s\n\n",text_size1, navn_paa_ori_fil);
+    printf("\nOri string: %d størrelse \n%s\n\n",text_size1, navn_paa_ori_fil);
     printf("Der er %d punktom i string\n\n", tæller_punktom);
-    printf("%d %s\n\n",text_size2, navn_paa_ny_fil);
+    printf("Rettet string: %d størrelse \n%s\n\n",text_size2, navn_paa_ny_fil);
 
     return 0;
 }
