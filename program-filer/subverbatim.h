75bcdd2d3029312ec0519e436a8f1471886bcee1
granquist8 <granquist8@gmail.com>
2020-11-30 10:18:04 +0100

    opdatering verbatim

--- program-filer/subverbatim.h
+++ program-filer/subverbatim.h
@@ -4,8 +4,9 @@
 #include <string.h>
 #include <stdbool.h>
 
-void verbatim(char arr1[][40], char arr2[][40])
+char **verbatim(char arr1[][40], char arr2[][40])
 {
+    //struct PlagMatch StrVerbatim;
     char verb_arr2[1000][40]; //Output arrayet
     int size_of_arr1 = sizeof(arr1);
     int q = 0;
@@ -32,8 +33,6 @@ void verbatim(char arr1[][40], char arr2[][40])
                     q++;                           //Så er q antal tegn ens
                     if (q == (int)strlen(arr1[i])) //Hvis der er lige så mange ens tegn, som der er i sætningen
                     {
-
-                        //printf("Its the same!\n");
                         for (int l = 0; l < (int)strlen(arr1[i]); l++)
                         {
                             verb_arr2[m][l] = arr1[i][l]; //Så bliver sætningen sat ind i det nye array
@@ -45,8 +44,9 @@ void verbatim(char arr1[][40], char arr2[][40])
         }
     }
 
-    for (int k = 0; k <= (int)size_of_arr1; k++)
+    /*for (int k = 0; k <= (int)size_of_arr1; k++)
     {
         printf("%s\n", verb_arr2[k]);
-    }
+    } */
+    return verb_arr2;
 }
