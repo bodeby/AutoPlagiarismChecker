69421904c6a5b66ea6b65c6f7345699c7937a4bf
tgrenaae <tgrena20@student.aau.dk>
2020-12-03 09:00:28 +0100

    verbatim cleanup
    cleanup of code
    changed array to dynamic allocation in able to return it to main.c.

--- program-filer/subverbatim.h
+++ program-filer/subverbatim.h
@@ -4,40 +4,28 @@
 #include <string.h>
 #include <stdbool.h>
 
-char **verbatim(char arr1[][40], char arr2[][40])
-{
-    //struct PlagMatch StrVerbatim;
-    char verb_arr2[1000][40]; //Output arrayet
+char **verbatim(char arr1[][40], char arr2[][40]) {
+    char **verb_arr2 = malloc(100 * sizeof(char*)); //Output arrayet
+    if(verb_arr2 == NULL){
+        exit(EXIT_FAILURE);
+    }
     int size_of_arr1 = sizeof(arr1);
-    int q = 0;
-    int m = 0;
-    //int largest_array = 0;
-
-    /*for (int i = 0; i < (int)size_of_arr1 + 1; i++)
-    {
-        if (largest_array < (int)strlen(arr1[i]))
-        {
-            largest_array = (int)strlen(arr1[i]);
-        }
-    } Muligvis ligegyldig kode */
-
-    for (int i = 0; i <= (int)size_of_arr1; i++) //i styrer sætninger i det første array
-    {
-        for (int n = 0; n <= (int)size_of_arr1; n++) //n styrer sætninger i det andet array
-        {
-            q = 0;                                              //q er antal tegn, der er ens i sætningerne
-            for (int j = 0; j <= (int)strlen(arr1[i]) - 1; j++) //j styrer tegn i begge sætninger
-            {
-                if (arr1[i][j] == arr2[n][j]) //Hvis tegnet er det samme i begge sætninger
-                {
-                    q++;                           //Så er q antal tegn ens
-                    if (q == (int)strlen(arr1[i])) //Hvis der er lige så mange ens tegn, som der er i sætningen
-                    {
-                        for (int l = 0; l < (int)strlen(arr1[i]); l++)
-                        {
-                            verb_arr2[m][l] = arr1[i][l]; //Så bliver sætningen sat ind i det nye array
+    int countchar = 0;
+    int verbindex = 0;
+    for(int k = 0; k < 100; k++){
+        verb_arr2[k] = malloc(40 * sizeof(char));
+    }
+    for (int i = 0; i <= (int)size_of_arr1; i++) { //i styrer sætninger i det første array
+        for (int n = 0; n <= (int)size_of_arr1; n++) { //n styrer sætninger i det andet array
+            countchar = 0;                                              //countchar er antal tegn, der er ens i sætningerne
+            for (int j = 0; j <= (int)strlen(arr1[i]) - 1; j++) { //j styrer tegn i begge sætninger
+                if (arr1[i][j] == arr2[n][j]){ //Hvis tegnet er det samme i begge sætninger
+                    countchar++;                           //Så er countchar antal tegn ens
+                    if (countchar == (int)strlen(arr1[i])) { //Hvis der er lige så mange ens tegn, som der er i sætningen
+                        for (int l = 0; l < (int)strlen(arr1[i]); l++) {
+                            verb_arr2[verbindex][l] = arr1[i][l]; //Så bliver sætningen sat ind i det nye array
                         }
-                        m++;
+                        verbindex++;
                     }
                 }
             }
