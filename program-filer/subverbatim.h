66e6adc5ba6172e04587c0fdb8616c884005536c
bodeby <thorbensen@gmail.com>
2020-12-17 16:54:14 +0100

    formatting på output

--- program-filer/subverbatim.h
+++ program-filer/subverbatim.h
@@ -21,7 +21,7 @@ int nverbatim(PlagMatch verbatim[], char **sentences_one, char **sentences_two, 
             int diff = editDist(sentences_one[i], sentences_two[j]);
             if (diff == 0)
             {
-                printf("Match at (i: %d, j: %d) - k = %d\n", i, j, k);
+                printf("Match at (i: %d, j: %d) - k = %d\n", i+1, j+1, k);
                 PlagMatch match = createPlagMatch(sentences_one[i], sentences_two[j], 0, i + 1, 0, j + 1);
                 plagAppend(verbatim, k, match, &size, &capacity);
                 k++;
